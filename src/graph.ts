b87115ba7bd785abed3bf3ed7f434b32f4f90431
Ian Homer <code@ianhomer.com>
2025-07-04 11:05:05 +0100

    test: simplify tests and use JS import to validate type

--- src/graph.ts
+++ src/graph.ts
@@ -2,9 +2,6 @@ import { GraphNode } from "./node";
 import { GraphLink } from "./link";
 
 export interface Graph {
-  id: string;
-  label: string;
   nodes: GraphNode[];
   links: GraphLink[];
-  directed?: boolean;
 }
--- src/link.ts
+++ src/link.ts
@@ -1,5 +1,4 @@
 export interface GraphLink {
-  id: string;
   source: string;
   target: string;
   label?: string;
