8e8e0e48462c68f25a87f24c84657fa4c76553b2
Ian Homer <code@ianhomer.com>
2025-07-11 10:49:37 +0100

    fix: add domain property to graph

--- src/types.ts
+++ src/types.ts
@@ -1,26 +1,27 @@
 export interface Graph {
+  domain?: string;
   id?: string;
   label?: string;
-  nodes: { [name: string]: Node };
   links: Link[];
+  nodes: { [name: string]: Node };
 }
 
 export interface Node {
-  label?: string;
-  type?: string;
   aliases?: Array<string>;
+  label?: string;
+  meta?: { [name: string]: string };
+  type?: string;
   value?: number;
   weights?: { [name: string]: number };
-  meta?: { [name: string]: string };
 }
 
 export interface Link {
   id?: string;
-  source: string;
-  type?: string;
-  target: string;
   label?: string;
+  meta?: { [name: string]: string };
+  source: string;
+  target: string;
+  type?: string;
   value?: number;
   weights?: { [name: string]: number };
-  meta?: { [name: string]: string };
 }
